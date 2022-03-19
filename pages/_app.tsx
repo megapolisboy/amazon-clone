ad8cd23df346af55a7b44e5cefba3b45a3d568b9
vdev3094 <66820680+vdev3094@users.noreply.github.com>
2022-03-19 14:34:00 +0200

    provider ts bug fixed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,9 +5,9 @@ import { Provider } from "react-redux";
 import store, { persistor } from "../app/store";
 import { PersistGate } from "redux-persist/integration/react";
 
-function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <SessionProvider session={session}>
+    <SessionProvider session={pageProps.session}>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <Component {...pageProps} />
